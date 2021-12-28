071b03de53c5c7cba8edbd2b261c4021abdbdd9e
Jason Shive <jshive@glytecsystems.com>
2021-12-28 11:17:11 -0500

    Corrected tag

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resource-group" {
   location = var.resource-location
   name     = local.resource-group-name
-  tags     = merge(local.common-tags, tomap({ "type" = "storage-account" }))
+  tags     = merge(local.common-tags, tomap({ "type" = "resource-group" }))
 }
